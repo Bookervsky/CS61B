4c93899ec1154796ce55016c3d1ab4decf0df9e2
Bookervsky <booklv123@gmail.com>
2023-04-12 16:33:04 +0800

    sick of this

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -1,5 +1,16 @@
 import static org.junit.Assert.*;
 import org.junit.Test;
 public class TestArrayDequeGold {
-
+    @Test
+    public void testA(){
+        StudentArrayDeque<Integer> s1 = new StudentArrayDeque<>();
+        s1.addFirst(StdRandom.uniform(9));
+        s1.addFirst(StdRandom.uniform(9));
+        s1.removeLast();
+        ArrayDequeSolution<Integer> s2 = new ArrayDequeSolution<>();
+        s2.addFirst(StdRandom.uniform(9));
+        s2.addLast(StdRandom.uniform(9));
+        s2.removeFirst();
+        assertEquals(s1.get(0),s2.getRecursive(0));
+    }
 }
